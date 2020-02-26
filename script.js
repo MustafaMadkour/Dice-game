8f9424147b782c3a8ba8130541b38b7eddd5aedc
Mustafa Madkour <madkour2790@gmail.com>
2020-02-26 22:50:34 +0200

    bugs-fixed

--- script.js
+++ script.js
@@ -38,10 +38,11 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 });
 
 
+document.querySelector('.btn-new').addEventListener('click', init);
 
 
 function nextPlayer(){
-    (playerTurn === 0) ? playerTurn = 1: playerTurn = 0;
+    (playerTurn == 0) ? playerTurn = 1: playerTurn = 0;
     roundScore = 0;
     document.getElementById('current-0').textContent = "0";
     document.getElementById('current-1').textContent = "0";
@@ -51,13 +52,12 @@ function nextPlayer(){
     document.getElementById('dice-2').style.display = "none";
 }
 
-document.querySelector('.btn-new').addEventListener('click', init());
 
 function init(){
     scores = [0, 0];
     playerTurn = 0;
     roundScore = 0;
-    gameplay = 0;
+    gameplay = 1;
     document.getElementById('dice-1').style.display = "none";
     document.getElementById('dice-2').style.display = "none";
     document.getElementById('score-0').textContent = "0";
